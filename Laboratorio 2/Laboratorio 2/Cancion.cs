0e1b4bbce81631446879b6de3ff74ec89e549dc2
Carmen Benavente <cbenavente@miuandes.cl>
2020-03-28 14:39:41 -0300

    Class Cancion

---
+++ Laboratorio 2/Laboratorio 2/Cancion.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio_2
+{
+    public class Cancion
+    {
+        public string Name;
+        public string Album;
+        public string Artist;
+        public string Genre;
+
+        public Cancion(string name, string album, string artist, string genre)
+        {
+            this.Name = name;
+            this.Album = album;
+            this.Artist = artist;
+            this.Genre = genre;
+        }
+
+        public void Informacion()
+        {
+            Console.WriteLine("genero: {0}, artista: {1}, album: {2}, nombre: {3}", this.Genre, this.Artist, this.Album, this.Name);
+        }
+    }
+    }
---
+++ Laboratorio 2/Laboratorio 2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var cancion = new Cancion("Stairway to Heaven", "Led Zeppelin IV", "Led Zeppelin","Hard Rock");
+            cancion.Informacion();
+            Console.ReadLine();
+        }
+    }
+}
